4a814602183c7cb2c8ed3986852a7395ce2f6a57
cbdyzj <cbdyzj@jianzhao.org>
2019-02-19 09:23:59 +0800

    Amend

--- proxy.ts
+++ proxy.ts
@@ -15,4 +15,6 @@ function route(req: IncomingMessage, res: ServerResponse) {
     proxy.web(req, res, { target: API_URL })
 }
 
-http.createServer(route).listen(5000, () => console.log('Proxying on 5000'))
+if (require.main === module) {
+    http.createServer(route).listen(5000, () => console.log('Proxying on 5000'))
+}
