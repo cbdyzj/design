0a1d37d3a2cedb5c45a1d8e509d04a04f5a08a56
cbdyzj <cbdyzj@jianzhao.org>
2019-02-22 19:52:19 +0800

    Amend

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Hello from "./component/hello"
 import Main from "./component/main"
+import Bind2 from "./component/bind2/bind2"
 import './app.css'
 
 export default () => (
@@ -12,7 +13,7 @@ export default () => (
                 <ul>
                     <li><Link to="/me">Me</Link></li>
                     <li><Link to="/hi/Ada">Ada</Link></li>
-                    <li><Link to="/hi/Alice">Alice</Link></li>
+                    <li><Link to="/bind2">Bind2</Link></li>
                     <li><Link to="/main">Main</Link></li>
                 </ul>
             </nav>
@@ -21,6 +22,7 @@ export default () => (
             <Route path="/me" component={() => <Hello name="Cbdy" />} />
             <Route path="/hi/:name" component={({ match }) => <Hello name={match.params.name} />} />
             <Route path="/main" component={() => <Main />} />
+            <Route path="/bind2" component={() => <Bind2 />} />
         </div>
     </Router>
 )
---
+++ src/component/bind2/bind2.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import './bind2.css'
+
+interface Props { }
+interface State { }
+
+export default class extends React.Component<Props, State> {
+
+    componentDidMount(): void {
+        const p = document.querySelector('p')
+        const input = document.querySelector('input')
+        input.addEventListener('input', ({ target }) => mo.value = target['value'])
+        const mo = new Proxy({ value: '' }, {
+            set: (target, property, value) => {
+                target[property] = value
+                input.value = value
+                p.innerText = JSON.stringify(target)
+                return true
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="bind2">
+                <h1>双向绑定哟</h1>
+                <form>
+                    <label htmlFor="bind2"/>
+                    <input type="text" id="bind2" name="value" placeholder="输入吧；）"/>
+                </form>
+                <p>{''}</p>
+            </div>
+        )
+    }
+}
