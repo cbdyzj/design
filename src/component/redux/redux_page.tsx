74e6723ebf179f446323ed71b3fe7d7b707f2e73
cbdyzj <cbdyzj@jianzhao.org>
2019-08-27 12:41:30 +0800

    Make sure Redux route page props

--- src/component/redux/redux_page.tsx
+++ src/component/redux/redux_page.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 import PageTurner from './page_turner'
 import Book from './book'
+import { connect } from 'react-redux';
 
-function ReduxPage() {
+function ReduxPage(props) {
+    const { match } = props
     return (
         <div>
+            <p style={{ margin: '20px 25px' }}>Path: <b>{match.path}</b></p>
             <Book />
             <PageTurner />
         </div>
     )
 }
 
-export default ReduxPage
+export default connect()(ReduxPage)
