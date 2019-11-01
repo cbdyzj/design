de63c229d6b2863311e27583648a4657e44a0625
cbdyzj <cbdyzj@jianzhao.org>
2019-11-01 19:37:50 +0800

    Amend

--- src/components/antd/ant_table.tsx
+++ src/components/antd/ant_table.tsx
@@ -10,6 +10,9 @@ const defaultTableData = Array.from({ length: 7 }).map((_, i) => ({
     sort: i,
 }))
 
+// 生效延迟
+const DELAY = 1500
+
 function AntTable() {
 
     const [data, setData] = useState(defaultTableData)
@@ -20,43 +23,54 @@ function AntTable() {
         return <Button onClick={infoContent}>操作</Button>
     }
 
-    function changeOrder(id, ev) {
-       const task =  setTimeout(() => {
+    function applySort(id, sort) {
+        return () =>{
             setData(data => {
-                changeQueue.current.delete(id)
+                changeQueue.current.clear()
                 return data.map(it => {
                     if(it.id === id){
-                        it.sort = ev
+                        it.sort = sort
                     }
                     return it
                 })
             })
-        }, 1000)
-        const lastTask = changeQueue.current.get(id);
-        if(lastTask !== (void 0)){
-            clearTimeout(lastTask)
         }
-        changeQueue.current.set(id, task)
+    }
+
+    function changeOrder(id, ev) {
+        const sortTask = applySort(id, ev)
+        const taskHandle = setTimeout(sortTask, DELAY)
+        const theItem = changeQueue.current.get(id);
+        if(theItem !== (void 0)) {
+            clearTimeout(theItem.taskHandle)
+        }
+        changeQueue.current.set(id, {
+            taskHandle,
+            sortTask,
+        })
+    }
+
+    function handleSortInputBlur(id) {
+        const theItem = changeQueue.current.get(id);
+        if(theItem !== (void 0)) {
+            clearTimeout(theItem.taskHandle)
+            theItem.sortTask()
+        }
     }
 
     function renderSortInput(text, record) {
         return (
             <InputNumber defaultValue={text.sort}
+                         onBlur={() => handleSortInputBlur(text.id)}
                          onChange={(ev) => changeOrder(text.id, ev)}/>
         )
     }
 
     const tableColumns: any = [
-        { title: 'ID', dataIndex: 'id', key: 'id', width: '20%', align: 'center',},
-        { title: '内容', dataIndex: 'content', key: 'content', width: '35%',},
-        { title: '操作', key: 'operation', render: renderOpButton, width: '20%', align: 'center',},
-        {
-            title: '排序',
-            key: 'sort',
-            render: renderSortInput,
-            width: '25%',
-            align: 'center',
-        },
+        { title: 'ID', dataIndex: 'id', key: 'id', width: '20%', align: 'center' },
+        { title: '内容', dataIndex: 'content', key: 'content', width: '35%' },
+        { title: '操作', key: 'operation', render: renderOpButton, width: '20%', align: 'center' },
+        { title: '排序', key: 'sort', render: renderSortInput, width: '25%', align: 'center' },
     ]
 
     data.sort((a, b) => a.sort - b.sort)
