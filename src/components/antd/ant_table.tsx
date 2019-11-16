9e5917f6178efb43ac5cf540e081a10695f1c5b8
cbdyzj <cbdyzj@jianzhao.org>
2019-11-16 18:04:50 +0800

    Amend

--- src/components/antd/ant_table.tsx
+++ src/components/antd/ant_table.tsx
@@ -1,10 +1,10 @@
 import React, {useRef, useState} from 'react'
 
-import { Button, Table, Modal, InputNumber } from 'antd'
+import {Button, Table, Modal, InputNumber, Popconfirm, message} from 'antd'
 
 import style from './style.less'
 
-const defaultTableData = Array.from({ length: 7 }).map((_, i) => ({
+const defaultTableData = Array.from({length: 7}).map((_, i) => ({
     id: i,
     content: `内容${i}`,
     sort: i,
@@ -19,16 +19,26 @@ function AntTable() {
     const changeQueue = useRef(new Map())
 
     function renderOpButton(text, record) {
-        const infoContent = () => Modal.info({ content: record.content })
-        return <Button onClick={infoContent}>操作</Button>
+        const infoContent = () => Modal.info({content: record.content})
+        return (
+            <Popconfirm
+                onConfirm={infoContent}
+                okText="确认"
+                cancelText="取消"
+                title="确认操作？">
+                <Button
+                    type="link">操作
+                </Button>
+            </Popconfirm>
+        )
     }
 
     function applySort(id, sort) {
-        return () =>{
+        return () => {
             setData(data => {
                 changeQueue.current.clear()
                 return data.map(it => {
-                    if(it.id === id){
+                    if (it.id === id) {
                         it.sort = sort
                     }
                     return it
@@ -41,7 +51,7 @@ function AntTable() {
         const sortTask = applySort(id, ev)
         const taskHandle = setTimeout(sortTask, DELAY)
         const theItem = changeQueue.current.get(id);
-        if(theItem !== (void 0)) {
+        if (theItem !== (void 0)) {
             clearTimeout(theItem.taskHandle)
         }
         changeQueue.current.set(id, {
@@ -52,7 +62,7 @@ function AntTable() {
 
     function handleSortInputBlur(id) {
         const theItem = changeQueue.current.get(id);
-        if(theItem !== (void 0)) {
+        if (theItem !== (void 0)) {
             clearTimeout(theItem.taskHandle)
             theItem.sortTask()
         }
@@ -67,24 +77,54 @@ function AntTable() {
     }
 
     const tableColumns: any = [
-        { title: 'ID', dataIndex: 'id', key: 'id', width: '20%', align: 'center' },
-        { title: '内容', dataIndex: 'content', key: 'content', width: '35%' },
-        { title: '操作', key: 'operation', render: renderOpButton, width: '20%', align: 'center' },
-        { title: '排序', key: 'sort', render: renderSortInput, width: '25%', align: 'center' },
+        {title: 'ID', dataIndex: 'id', key: 'id', width: '20%', align: 'center'},
+        {title: '内容', dataIndex: 'content', key: 'content', width: '35%'},
+        {title: '操作', key: 'operation', render: renderOpButton, width: '20%', align: 'center'},
+        {title: '排序', key: 'sort', render: renderSortInput, width: '25%', align: 'center'},
     ]
 
     data.sort((a, b) => a.sort - b.sort)
 
+    const [selectedTableRowKeys, setSelectedTableRowKeys] = useState([])
+    const [selectedTableRows, setSelectedTableRows] = useState([])
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            setSelectedTableRowKeys(selectedRowKeys)
+            setSelectedTableRows(selectedRows)
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        selectedRowKeys: selectedTableRowKeys,
+        columnTitle: ' ',
+    }
+
+    function exportFile() {
+        alert(JSON.stringify(selectedTableRows))
+        setSelectedTableRowKeys([])
+        setSelectedTableRows([])
+    }
+
     return (
-        <Table className={style['ant-table']}
-            pagination={false}
-            style={{ margin: '1rem' }}
-            dataSource={data}
-            onChange={() => {}}
-            columns={tableColumns}
-            size="small"
-            rowKey="id"
-        />
+        <div>
+            <Button
+                onClick={() => message.info('导入')}
+                className={style.export}>导入</Button>
+            <Button
+                onClick={exportFile}
+                className={style.export}>导出</Button>
+            <Table
+                rowSelection={rowSelection}
+                className={style['ant-table']}
+                pagination={false}
+                style={{margin: '1rem'}}
+                dataSource={data}
+                onChange={() => {
+                }}
+                columns={tableColumns}
+                size="small"
+                rowKey="id"
+            />
+        </div>
     )
 }
 
