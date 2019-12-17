3341be0d5a91de88749fff2f38c6bff8b160a133
cbdyzj <cbdyzj@jianzhao.org>
2019-12-17 17:47:20 +0800

    Amend

--- src/pages/antd/antd.tsx
+++ src/pages/antd/antd.tsx
@@ -1,27 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { Button, Row, message } from 'antd'
 
 import AntTable from '../../components/antd/ant_table'
 import AntPagination from '../../components/antd/ant_pagination'
 import SuggestInput from '../../components/antd/suggest_input'
-import { sleep } from '../../util/schedule'
-
-async function countNumber(n) {
-    for (let i = 1; i <= n; i++) {
-        await sleep(i * 1000)
-        message.info(`--- ${i} ---`)
-    }
-}
 
 function Antd() {
     function onClickButton() {
         message.info('Welcome Ant Design!')
     }
-
-    useEffect(() => {
-        countNumber(7)
-    }, [])
 
     return (
         <div style={{ width: '60%', margin: 'auto' }}>
