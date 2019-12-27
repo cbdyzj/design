ade50c9273d5bd572e491ffa61120e4c20413cd2
cbdyzj <cbdyzj@jianzhao.org>
2019-12-27 14:01:36 +0800

    Amend

---
+++ template.plugin.js
@@ -0,0 +1,26 @@
+const MiniHtmlWebpackPlugin = require('mini-html-webpack-plugin')
+const { generateJSReferences } = MiniHtmlWebpackPlugin
+
+function template({ publicPath, js }) {
+    const jsTags = generateJSReferences({
+        files: js,
+        publicPath
+    })
+    return `<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TSX and CSS</title>
+</head>
+<body>
+    <div id="app"></div>
+    ${jsTags}
+</body>
+</html>`
+}
+
+module.exports = [
+    new MiniHtmlWebpackPlugin({ template })
+]
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,31 @@
-const MiniHtmlWebpackPlugin = require('mini-html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
-
-function template({publicPath, js}) {
-    const jsTags = MiniHtmlWebpackPlugin.generateJSReferences({
-        files: js,
-        publicPath
-    })
-    return `<!DOCTYPE html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TSX and CSS</title>
-</head>
-<body>
-    <div id="app"></div>
-    ${jsTags}
-</body>
-</html>`
-}
+const templatePlugin = require('./template.plugin')
 
 const config = {
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: [{loader: 'ts-loader'}],
+                use: [{ loader: 'ts-loader' }],
                 exclude: /node_modules/,
             }, {
                 test: /\.(png|jpg|jpeg|gif)$/,
-                use: [{loader: 'file-loader', options: {outputPath: 'assets'}}],
+                use: [{ loader: 'file-loader', options: { outputPath: 'assets' } }],
             }, {
                 test: /\.(css|less)$/,
                 use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader', options: {modules: true}},
-                    {loader: 'less-loader'},
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true } },
+                    { loader: 'less-loader' },
                 ],
                 exclude: [path.resolve(__dirname, 'node_modules')]
             }, {
                 test: /\.(css|less)$/,
                 use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                    {loader: 'less-loader'},
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' },
                 ],
                 include: [path.resolve(__dirname, 'node_modules')]
             }
@@ -56,7 +36,7 @@ const config = {
     },
     plugins: [
         new CopyPlugin(['favicon.ico']),
-        new MiniHtmlWebpackPlugin({ template }),
+        ...templatePlugin,
     ],
     optimization: {
         splitChunks: {
