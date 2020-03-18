9cacc8f3672880a10175b609b3baa4695f0a8b5c
cbdyzj <cbdyzj@jianzhao.org>
2020-03-18 16:44:42 +0800

    Amend

--- src/pages/react_hook/react_hook.tsx
+++ src/pages/react_hook/react_hook.tsx
@@ -1,20 +1,38 @@
 import React, { useCallback, useState } from 'react'
 
-function ReactHook(props) {
-    const now = new Date().toISOString()
-    const [info, setInfo] = useState('?')
+function ContentRender(props) {
+    const content = props.content || ''
+    const split = content.split('{}')
+    const elements = []
 
-    const memoCallback = useCallback(() => setInfo(now), [now])
+    elements.push(<span key={'span-0'}>{split[0]}</span>)
 
-    function handleClick() {
-        memoCallback()
+    if (split.length > 1) {
+        split.slice(1).forEach((item, index) => {
+            elements.push(<input key={'input-' + index}/>)
+            elements.push(<span key={'span-' + (index + 1)}>{item}</span>)
+        })
     }
+
+    return (
+        <div style={{ margin: '8px 0' }}>{elements}</div>
+    )
+}
+
+function ReactHook(props) {
+    const [content, setContent] = useState('')
+
+    function handleInputChange(ev) {
+        setContent(ev.target.value)
+    }
+
 
     return (
         <div style={{ margin: '24px' }}>
             <h1>React hook</h1>
-            <p>{info}</p>
-            <button onClick={handleClick}>Callback</button>
+            <input value={content}
+                   onChange={handleInputChange}/>
+            <ContentRender content={content}/>
         </div>
     )
 }
