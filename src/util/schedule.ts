7b95bc383b3d2a27928d19dfe083dfbc3614a9a4
cbdyzj <cbdyzj@jianzhao.org>
2020-12-04 14:15:59 +0800

    Add iframe demo

---
+++ src/util/schedule.ts
@@ -0,0 +1,24 @@
+export function debounce(func, delay) {
+    let timer
+
+    return function (...args) {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => func(...args), delay)
+    }
+}
+
+export function throttle(func, delay) {
+    let wait
+    return function (...args) {
+        if (wait) {
+            return
+        }
+        wait = true
+        setTimeout(() => {
+            wait = false
+            func(...args)
+        }, delay)
+    }
+}
