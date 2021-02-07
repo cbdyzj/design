eb2f680b0d5313cbf7de48153495a12247f7843c
cbdyzj <cbdyzj@jianzhao.org>
2021-02-07 23:14:11 +0800

    Fix typo

--- src/pages/layout2/Layout2.jsx
+++ src/pages/layout2/Layout2.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Extract from '@/components/extract/extract'
+import Extract from '@/components/extract/Extract'
 import style from './style.module.less'
 
 export default function Layout2() {
